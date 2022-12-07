11fdd76364499c754ee3b5205123a769b1a1f457
Will Saults <will@duffl.com>
2022-12-07 13:59:51 -0600

    Complete S01E04

--- QuizEngine/QuizEngine/Flow.swift
+++ QuizEngine/QuizEngine/Flow.swift
@@ -8,12 +8,13 @@
 import Foundation
 
 protocol Router {
-    func routeTo(question: String, answerCallback: @escaping (String) -> Void)
+    typealias AnswerCallback = (String) -> Void
+    func routeTo(question: String, answerCallback: @escaping AnswerCallback)
 }
 
 class Flow {
-    let router: Router
-    let questions: [String]
+    private let router: Router
+    private let questions: [String]
     
     init(questions: [String], router: Router) {
         self.questions = questions
@@ -22,11 +23,18 @@ class Flow {
     
     func start() {
         if let firstQuestion = questions.first {
-            router.routeTo(question: firstQuestion) { [weak self] _ in
-                guard let strongSelf = self else { return }
-                let firstQuestionIndex = strongSelf.questions.firstIndex(of: firstQuestion)!
-                let nextQuestion = strongSelf.questions[firstQuestionIndex + 1]
-                strongSelf.router.routeTo(question: nextQuestion) { _ in }
+            router.routeTo(question: firstQuestion, answerCallback: routeNext(from: firstQuestion))
+        }
+    }
+    
+    private func routeNext(from question: String) -> Router.AnswerCallback {
+        { [weak self] _ in
+            guard let strongSelf = self else { return }
+            if let currentQuestionIndex = strongSelf.questions.firstIndex(of: question) {
+                if currentQuestionIndex + 1 < strongSelf.questions.count {
+                    let nextQuestion = strongSelf.questions[currentQuestionIndex + 1]
+                    strongSelf.router.routeTo(question: nextQuestion, answerCallback: strongSelf.routeNext(from: nextQuestion))
+                }
             }
         }
     }
