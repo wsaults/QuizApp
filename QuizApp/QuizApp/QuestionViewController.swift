3c5cd20143d6256be0acc13dc06fabb27f728fba
Will Saults <will.saults@rewardstyle.com>
2023-01-13 10:59:20 -0600

    Complete: S01E06

---
+++ QuizApp/QuizApp/QuestionViewController.swift
@@ -0,0 +1,38 @@
+//
+//  QuestionViewController.swift
+//  QuizApp
+//
+//  Created by Will Saults on 1/13/23.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var headerLabel: UILabel!
+
+    private var question = ""
+    private var options = [String]()
+
+    convenience init(question: String, options: [String]) {
+        self.init()
+        self.question = question
+        self.options = options
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        headerLabel.text = question
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        options.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = options[indexPath.row]
+        return cell
+    }
+}
