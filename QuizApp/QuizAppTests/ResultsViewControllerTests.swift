1c3eab5805b4d371bb8b41318b7456092d08d192
Will Saults <will.saults@rewardstyle.com>
2023-02-23 11:59:41 -0600

    Complete: S01E09

---
+++ QuizApp/QuizAppTests/ResultsViewControllerTests.swift
@@ -0,0 +1,49 @@
+//
+//  ResultsViewControllerTests.swift
+//  QuizAppTests
+//
+//  Created by Will Saults on 2/23/23.
+//
+
+@testable import QuizApp
+import XCTest
+
+class ResultsViewControllerTests: XCTestCase {
+
+    func test() {
+        XCTAssertEqual(makeSUT(summary: "a summary").headerLabel.text, "a summary")
+    }
+
+    func test_viewDidLoad_withoutAnswers_doesNotRenderAnswers() {
+        XCTAssertEqual(makeSUT(answers: []).tableView.numberOfRows(inSection: 0), 0)
+        XCTAssertEqual(makeSUT(answers: [makeDummyAnswer()]).tableView.numberOfRows(inSection: 0), 1)
+    }
+
+    func test_viewDidLoad_withCorrectAnswer_rendersCorrectAnswerCell() {
+        let sut = makeSUT(answers: [makeDummyAnswer(isCorrect: true)])
+
+        let cell = sut.tableView.cell(at: 0) as? CorrectAnswerCell
+
+        XCTAssertNotNil(cell)
+    }
+
+    func test_viewDidLoad_withWrongAnswer_rendersWrongnswerCell() {
+        let sut = makeSUT(answers: [makeDummyAnswer(isCorrect: false)])
+
+        let cell = sut.tableView.cell(at: 0) as? WrongAnswerCell
+
+        XCTAssertNotNil(cell)
+    }
+
+    // MARK: Helpers
+
+    func makeSUT(summary: String = "", answers: [PresentableAnswer] = []) -> ResultsViewController {
+        let sut = ResultsViewController(summary: summary, answers: answers)
+        sut.loadViewIfNeeded()
+        return sut
+    }
+
+    func makeDummyAnswer(isCorrect: Bool = false) -> PresentableAnswer {
+        PresentableAnswer(isCorrect: isCorrect)
+    }
+}
